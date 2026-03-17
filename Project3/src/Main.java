a8b7d50acc9039c409707504672a045bcf5fc161
CJ3031993 <CJ3031993@LAB01-PC08.ifspcjoad.ifspcjo.edu.br>
2026-03-17 19:47:08 -0300

    Project3

---
+++ Project3/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numero1, numero2, soma;
+		numero1 = 4;
+		numero2 = 3;
+		soma = numero1 + numero2;
+		
+		System.out.println("Soma = " + soma);
+		
+	}
+
+}
