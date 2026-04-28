4b40a7a9bf89cec0717c00f84dda85091b445949
CJ3031993 <CJ3031993@LAB01-PC01.ifspcjoad.ifspcjo.edu.br>
2026-04-28 20:06:54 -0300

    28/04/2026

--- ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/exceptionhandler/IfitnessExceptionHandler.java
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/exceptionhandler/IfitnessExceptionHandler.java
@@ -1,11 +1,14 @@
 package br.edu.ifspcjo.ads.web2.ifitness.exceptionhandler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -15,6 +18,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -48,6 +52,17 @@ public class IfitnessExceptionHandler extends ResponseEntityExceptionHandler{
 			errors.add(new Error(userMessage, developerMessage));
 		}
 		return errors;
+	}
+	
+	@ExceptionHandler({EmptyResultDataAccessException.class})
+	protected ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+			WebRequest request) {
+		String userMessage = messageSource.getMessage("resource.not-found", null, 
+			LocaleContextHolder.getLocale());
+		String developerMessage = Optional.ofNullable(ex.getCause()).orElse(ex).toString();
+		
+		List<Error> errors = Arrays.asList(new Error(userMessage, developerMessage));
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 
 	public static class Error{
--- ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/resource/UserResource.java
+++ ifitness/src/main/java/br/edu/ifspcjo/ads/web2/ifitness/resource/UserResource.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifspcjo.ads.web2.ifitness.domain.model.User;
 import br.edu.ifspcjo.ads.web2.ifitness.repository.UserRepository;
+import br.edu.ifspcjo.ads.web2.ifitness.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 
@@ -25,6 +28,9 @@ public class UserResource {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping
 	public List<User> findAll(){
@@ -45,5 +51,29 @@ public class UserResource {
 		}
 		return ResponseEntity.notFound().build();
 	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @Valid @RequestBody User user) {
+		User userSaved = userService.update(id, user);
+		return ResponseEntity.ok(userSaved);
+	}
+	
+	@PutMapping("/{id}/active")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void updateActiveProperty(@PathVariable Long id, @RequestBody Boolean active) {
+		userService.updateActiveProperty(id, active);
+	}
+
 	
 }
+
+
+
+
+
